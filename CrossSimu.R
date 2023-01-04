20fad67e418e97e6e74c5654f124176f46702c89
keyao14 <106634626+keyao14@users.noreply.github.com>
2023-01-04 09:38:14 +0800

    Update CrossSimu.R

--- CrossSimu.R
+++ CrossSimu.R
@@ -3,12 +3,12 @@ library(doParallel); cl <- makeCluster(50); registerDoParallel(cl)
 source('CrossTest.R')
 source('CrossModel.R')
 depend_packages = c('energy','pcaPP','testcorr')
-for(i in (c(0:4)*0.2)){
-  for(j in c(1:7)){
+for(i in c(1:7)){
+  for(j in (c(0:4)*0.2)){
     Err <- foreach(rep=1:500, .combine='cbind',.packages = depend_packages) %dopar%
       {
         
-        simu = gts(600,12,j,i)
+        simu = gts(600,12,i,j)
         I = simu$I
         J = simu$J
         pvalue=c(FOD(I,J,12,5,5,500),FOD_kendall(I,J,12,5,5,500),FOD_dcov(I,J,12,5,5,500), cc.test(I,J,max.lag=5)$pvqtilde[11])
@@ -16,7 +16,7 @@ for(i in (c(0:4)*0.2)){
       }
     
     power = rowMeans(Err<0.05)
-    print(c(j,i,power))
+    print(c(i,j,power))
   }
 }
 
