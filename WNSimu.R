ec887792e1c4716fae9bb10d2fc72263b9a55005
keyao14 <106634626+keyao14@users.noreply.github.com>
2023-10-06 17:51:50 +0800

    Update WNSimu.R

--- WNSimu.R
+++ WNSimu.R
@@ -5,15 +5,15 @@ source('WNModel.R')
 
 depend_packages = c('energy','pcaPP')
 for (i in c(1:3)) {
-  for(C in (c(0:5)*0.06)){
+  for(C in (c(0:5)*0.1)){
     n = 600
-    b = 10
+    w = 10
     m = 6
     Err <- foreach(rep=1:1000, .combine='cbind',.packages = depend_packages) %dopar%
       {
         X=sx(n,i,C)$X
         DX=c(X[2:n]-X[1:(n-1)],X[1]-X[n])
-        pvalue=c(wntest(X,b,m,5,500),wntest_kendall(X,b,m,5,500),wntest_dcov(X,b,m,5,500),Box.test(X,lag=5,type="Ljung-Box")[["p.value"]],Box.test(DX,lag=5,type="Ljung-Box")[["p.value"]])
+        pvalue=c(wntest(X,w,m,5,500),wntest_kendall(X,w,m,5,500),wntest_dcov(X,w,m,5,500),Box.test(X,lag=5,type="Ljung-Box")[["p.value"]],Box.test(DX,lag=5,type="Ljung-Box")[["p.value"]])
         pvalue
       }
     
